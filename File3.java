045e020bf697982db0844895d787cb6dd5aec1b6
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-28 15:56:28 +0530

    Add files via upload

---
+++ File3.java
@@ -0,0 +1,15 @@
+package Pack1;
+
+public class File3 {
+	public static void main(String[]args)
+	{
+		String name="suresh";
+		String hno="13/6";
+		String city="surampalem";
+		System.out.println("name="+name);
+		System.out.println("hno="+hno);
+		System.out.println("city="+city);
+		
+	}
+
+}
