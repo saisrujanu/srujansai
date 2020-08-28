b0175c1c180baf21c52f0bc9bcffa2af23e291f2
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-28 20:59:33 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,16 @@
+package pack2;
+import java.util.*;
+public class forloop {
+public static void main(String[] args) {
+	System.out.println("numbers fromm 1 to 10");
+	for(int j=0;j<=5;j++)
+	{
+	for(int i=1;i<=5;i++)
+	{
+		System.out.println(i);
+	}
+		
+	}
+	}
+
+}
