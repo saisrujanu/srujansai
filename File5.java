5bf2d6e90da6e4558e884976101838d739547d62
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-28 15:57:19 +0530

    Add files via upload

---
+++ File5.java
@@ -0,0 +1,16 @@
+package Pack1;
+import java.util.*;
+public class File5 {
+	public static void main(String[]args)
+	{
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter the length value=");
+		int l=obj.nextInt();
+		System.out.println("enter the breadth value ");
+		int b=obj.nextInt();
+		int a=l*b;
+		System.out.println("area of the rectangle="+a);
+	}
+	
+
+}
