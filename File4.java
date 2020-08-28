8b02e82bbd63346f912a57dde19a293c5b36f9ff
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-28 15:57:02 +0530

    Add files via upload

---
+++ File4.java
@@ -0,0 +1,14 @@
+package Pack1;
+import java.util.*;
+public class File4 {
+	public static void main(String[]args)
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the radius=");
+		int side=sc.nextInt();
+		int a=side*side;
+		System.out.println("area of square="+a);
+		
+	}
+
+}
