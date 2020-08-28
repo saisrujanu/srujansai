bf339ab26098bb606ce2dc3bf388bc359bbaa44c
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-28 20:59:09 +0530

    Add files via upload

---
+++ branchingstatements.java
@@ -0,0 +1,16 @@
+package pack2;
+import java.util.*;
+public class branchingstatements {
+	public static void main(String[]args)
+	{
+	Scanner obj=new Scanner(System.in);
+	System.out.println("enter the number:");
+	int x=obj.nextInt();
+	if (x%2==0)
+		System.out.println("the given number is even number:"+x);
+	else
+		System.out.println("the given number is odd number:"+x);
+	
+	}
+
+}
