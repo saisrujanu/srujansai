ed7e2a8167bed23db4d5649666df37aa0246a36e
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-08-31 13:25:52 +0530

    Add files via upload

---
+++ single.java
@@ -0,0 +1,14 @@
+package arrays;
+import java.util.*;
+public class single {
+	public static void main(String[] args) {
+		int a[]={10,20,30,40,50};
+		System.out.println("arrays values are:");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
