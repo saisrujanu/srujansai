128dadf53bde953f578b81270576400fbf95c218
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-01 16:33:27 +0530

    Add files via upload

---
+++ heptagon.java
@@ -0,0 +1,15 @@
+package formula_prgs;
+import java.util.*;
+public class heptagon {
+
+	public static void main(String[] args) {
+	Scanner obj=new Scanner(System.in);
+	System.out.println("Enter the length of a side:");
+	float s=obj.nextFloat();
+	float a =3.634f*s*s;
+	System.out.println("Area of heptagon:"+a);
+	
+
+	}
+
+}
