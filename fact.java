cee76b5f598f7ff7addcaf7fa7aebcc7c72499ef
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-01 16:03:33 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,16 @@
+package formula_prgs;
+import java.util.*;
+public class fact {
+public static  void main(String []args){
+	Scanner obj=new Scanner (System.in);
+	System.out.println("enter number:");
+	int =obj.nextInt();
+	int f=1;
+	for(int i=1;i<=5;i++);
+	{
+		f=f*i;
+	}
+	System.out.println("factorial="+f);
+	
+}
+}
