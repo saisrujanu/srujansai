4a87d9e803f3ac17c16e373b8025db56f80910ef
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-01 16:35:26 +0530

    Add files via upload

---
+++ hexagon.java
@@ -0,0 +1,9 @@
+package formula_prgs;
+import java.util.*;
+public class hexagon {
+public static void main(String []args){
+	c
+	double a=(3*Math.sqrt(3)*(s*s))/2;
+	System.out.println("Area of Hexagon:"+a);
+}
+}
