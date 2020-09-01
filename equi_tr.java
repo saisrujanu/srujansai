85d2b1af8e27646ad7172919e82e74b5fd6cbd4f
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-01 16:03:10 +0530

    Add files via upload

---
+++ equi_tr.java
@@ -0,0 +1,14 @@
+package formula_prgs;
+import java.util.*;
+public class equi_tr {
+	public static void main(String[] args) {
+	Scanner obj=new Scanner (System.in);
+	System.out.println("enter the side of a triangle=");
+	float s=obj.nextFloat();
+	float a=(1.37f*s*s);
+	System.out.println("Area of equivalateral triangle:"+a);
+	
+
+	}
+
+}
