d8bed9a6071dd1620d94d0a209bc94ab011ecb8c
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-03 19:52:33 +0530

    Add files via upload

---
+++ cel_far.java
@@ -0,0 +1,14 @@
+package formula_prgs2;
+import java.util.*;
+public class cel_far {
+	public static void main(String[] args) {
+	Scanner sc =new Scanner (System.in);
+	float f,c;
+		System.out.println("Enter the celsius temperature:");
+	c=sc.nextFloat();
+	f=((c*9/5))+32;
+	System.out.println("Farenhiet temperature is="+f);
+
+	}
+
+}
