41f348423994c7c74b654d01254fbdda477f6e1f
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-06 10:01:13 +0530

    Add files via upload

---
+++ rhombus.java
@@ -0,0 +1,15 @@
+package formula_prgs2;
+import java.util.*;
+public class rhombus {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 2 diagonals:");
+		float d1=sc.nextFloat();
+		float d2=sc.nextFloat();
+		float area=d1*d2*1/2;
+		System.out.println("Area of rhombus :"+area);
+	
+
+	}
+
+}
