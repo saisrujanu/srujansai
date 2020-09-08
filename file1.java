ba6b03e5013b6f9fb7a48bf63b2c4b1a35d7539f
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-08 18:50:54 +0530

    Add files via upload

---
+++ file1.java
@@ -0,0 +1,14 @@
+package pack5;
+public class file1 
+{
+	public static void main(String[] args) {
+		{
+			int a=20;
+			Integer i=Integer.valueOf (a);
+			Integer j=a;
+			System.out.println("a="+a+"i="+i+"j="+j);
+		}
+
+	}
+
+}
