f8f45190697f42168be177c77ada9461eea3491b
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-09 09:10:17 +0530

    Add files via upload

---
+++ prime_range.java
@@ -0,0 +1,23 @@
+package prgs3;
+import java.util.*;
+public class prime_range {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Enter 2 numbers:");
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		for (int i=n1+1;i<n2;i++)
+		{
+			int c=0;
+			for( int n=i;n>=i;n--)
+			{
+				if (i%n==0)
+				{c=c+1;}
+			}
+		if (c==2)
+			System.out.print(i+"");
+	   }
+
+	}
+
+}
