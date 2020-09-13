2641f769bc15710a0e4c91884793a0efa06b32ad
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-13 18:26:40 +0530

    Add files via upload

---
+++ pack43.java
@@ -0,0 +1,17 @@
+package finals;
+class A1
+{
+	static int x=1;
+	static void show()
+	{
+		System.out.println("Value="+x);
+	}
+}
+public class pack43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A1.show();
+	}
+
+}
