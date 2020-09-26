330a453f4cad23cd79f8e02b0c1bf8b7f3ca2f78
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2020-09-26 13:40:45 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,17 @@
+package prgs6;
+public class Pattern3 {
+	public static void main(String[] args) {
+		int n=5;
+		for (int i=1;i<=n;i++)
+		{
+			for (int j=n-1;j>=1;j--)
+			{System.out.print(" ");}
+			for (int j=1;j<=i;j++)
+			
+				{System.out.print("* ");}
+			System.out.println("");
+		}
+
+	}
+
+}
