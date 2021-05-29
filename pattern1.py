a865fd5920a2b7f5840ae7f9940eb4d85a360274
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2021-05-29 14:07:29 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,137 @@
+'''n=int(input())
+for i in range (1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range(n,0,-1):
+    t=n
+    for j in range(i,t+1):
+        print(j,end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range(1,n+1):
+    for j in range(n,i-1,-1):
+        print(j,end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for j in range(n,i,-1):
+        print("A",end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range (n,0,-1):
+    for s in range(n,i,-1):
+        print("",end=" ")
+    for j in range (1,i+1):
+        print(j,end=" ")
+    print()'''
+
+
+'''n=int(input())
+for i in range (n,0,-1):
+    for s in range(n,i,-1):
+        print(" ",end=" ")
+    for j in range (1,i+1):
+        print("*",end=" ")
+    print()'''
+
+
+
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for s in range(0,i,-1):
+        print(" ",end=" ")
+    for j in range (i,0,-1):
+        print("*",end=" ")
+    print()'''
+
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for s in range(n,n-1,-1):
+        print(" ",end=" ")
+    for j in range (i,0,-1):
+        print("*",end=" ")
+    print()'''
+
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for s in range(0,n+1):
+        print(" ",end=" ")
+    for j in range (i,i-1,-1):
+        print("*",end=" ")
+    print()'''
+
+
+
+
+
+'''n=int(input())
+for i in range (n,0,-1):
+    for s in range(n,i,-1):
+        print("",end=" ")
+    for j in range (1,i+1):
+        print("*",end=" ")
+    print()'''
+
+
+
+
+'''n=int(input())
+for i in range (1,n+1):
+    for s in range(1,n-i,+1):
+        print(" ",end=" ")
+    for j in range (1,2*i):
+        print("*",end=" ")
+    print()'''
+
+
+    
+
+n=int(input())
+for i in range (1,n+1):
+    for s in range(1,n-i,+1):
+        print(" ",end=" ")
+    for j in range (1,2*i):
+        print(j,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
