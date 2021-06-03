3ccb790a394eced3f5fc6d936c638e7660bf284e
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2021-06-03 09:19:57 +0530

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
 
-def min_max(n,data):
+"""def min_max(n,data):
     s,l=data[0],data[0]
     for i in data:
         if s>i:
@@ -15,12 +15,12 @@ def min_max(n,data):
 n=int(input())
 data=list(map(int,input().split()))
 mi,ma=min_max(n,data)
-print(mi,ma)
+print(mi,ma) """
 
 """
 min and max in a list
-5
-45 32 431 171 184 13
+6
+45 32 431 171 184 13 
 13 431
 
 
@@ -28,3 +28,136 @@ s=45>32----->s=32>13--->s13
 l=45<431--->l=431<13-->l=431
 
 using functions with return value """
+
+
+
+
+
+
+
+
+
+"""def findmin(n,data):
+    a,mc,l=data[0],0,[]
+    for i in data:
+        if i==a:
+            mc+=1
+        if i<a:
+            a=i
+            mc=1
+    for i in range(n):
+        if data[i]==a:
+            l.append(i)
+    l.insert(0,a)
+    l.insert(1,mc)
+    return l
+
+
+
+n=int(input())
+data=list(map(int,input().split()))
+minval=findmin(n,data)
+print(*minval)  """
+
+
+
+
+
+
+
+"""
+min and max in a list
+10
+45 32 431 171 184 13 21 13 33 13
+
+
+
+s=45>32----->s=32>13--->s13
+l=45<431--->l=431<13-->l=431
+
+using functions with return value """
+
+
+
+
+
+
+
+
+
+
+
+def findmin(n,data):#45 32  13 431 171 184 13 21 13 33 13
+    s,c=data[0],0
+    for i in data:
+        if s==i:
+            c+=1
+        if s>i:
+            s=i
+            c=1
+    ind=[s,c]        
+    for i in range(n):
+        if s==data[i]:
+            ind.append(i)
+    return ind  
+
+
+
+
+
+
+
+
+n=int(input())
+data=list(map(int,input().split()))
+minval=findmin(n,data)
+print(*minval)
+
+
+
+
+
+
+
+"""
+min and max in a list
+10
+45 32 13 431 171 184 13 21 13 33 13
+
+13 4 1 5 7 9 
+
+using functions with return value """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
