4de8436a317b06ef474b86f945c026ae661005b2
saisrujanu <70358813+saisrujanu@users.noreply.github.com>
2021-06-23 09:13:56 +0530

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,264 @@
+'''Strings:
+
+
+    this is python class
+    s[0]
+    s[1]------>'h'
+
+
+s=input()
+print(s)
+
+
+we can generete and we cant module or change the string.'''
+
+
+
+
+'''s=input()
+n=len(s)
+print(s)
+for i in range (n):
+    print(s[i],end=" ")
+print()
+for i in s:
+    print(i,end=" ")
+
+
+
+
+
+
+list(s)
+data=list(s)
+data
+s
+s.split()
+data=s.split()
+data
+
+data=str(s)
+data
+
+s=str(data)
+s
+
+
+data
+
+' 'join(data)
+data=list(s)
+data
+
+s
+data=list(s)
+data
+
+
+
+data[0]='t'
+data
+
+
+''join(data)
+
+
+ord('a')
+97
+
+
+chr(80)
+x" =num'''
+
+
+'''s=input()
+a=[0]*26
+for i in s:
+    print(i,end=" ")'''
+
+
+
+
+'''s=input()
+a=[0]*26
+for i in s:
+    print(ord(i))
+
+
+app key values:
+
+this is srujan class
+116
+104
+105
+115
+32
+105
+115
+32
+115
+114
+117
+106
+97
+110
+32
+99
+108
+97
+115
+115'''
+
+
+
+
+'''s=input()
+a=[0]*26
+for i in s:
+    if i!=' ':
+        a[(ord(i))-97]+=1
+for i in range(26):
+    if a[i]
+'''
+
+
+
+'''s=input()
+w=s.split()
+print(len(w))'''
+
+"""
+this is python class
+4
+"""
+
+
+
+
+'''s=input()
+data=s.split()
+for i in data:
+    print(len(i))'''
+
+
+
+"""
+this is python class
+4 2 6 5 
+"""
+
+
+
+
+'''s=input()
+data=s.split()
+l=0
+pos=0
+for i in range(len(data)):
+    if len (data[i]>1:
+            l=len(data[i])
+            pos=i
+
+print(pos+1,data[pos],1)'''
+
+
+
+
+
+
+
+
+
+"""
+this is python class
+
+1.ssalc nohtyp si siht
+2.siht si nohtyp ssalc
+3.class python is this'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
